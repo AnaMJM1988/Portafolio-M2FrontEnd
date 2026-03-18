d58392980532c81bdd0e06cd19d3f410d6b4b410
Ana Jaramillo <a.jaramillomoya@uandresbello.edu>
2026-03-18 20:36:52 -0300

    feat: crea función para recorrer array pronosticoSemanal y mostrarlo en una lista en detalle.html

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -67,9 +67,19 @@ const mostrarLugar=()=>{
 </div>
 `;
 
-LugarContainer.innerHTML=content;
+  LugarContainer.innerHTML=content;
 
 };
 mostrarLugar();
-//TODO: mostrar sección de pronóstico semanal
+//mostrar sección de pronóstico semanal
+const pronosticoContainer = document.getElementById('pronosticoSemanal');
+console.log(ciudadActual.pronosticoSemanal);
+ciudadActual.pronosticoSemanal.forEach((dia)=>{
+  const content =`
+          <li class="list-group-item">
+              <i class="bi ${ICONOS[dia.estado]}"></i> ${dia.dia} : ${dia.max}°C
+              </li>
+`;
+pronosticoContainer.innerHTML += content;
+});
 //TODO: mostrar estsadísticas semanales.
